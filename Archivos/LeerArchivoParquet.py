4da0e3d8593a9f63d0487067e7bc68998361768f
oscar.hernandez.barron.contractor <oscar.hernandez.barron.contractor@bbva.com>
2024-07-03 16:57:42 -0600

    Se adiciona ejemplo para lectura de archivo Parquet con pyarrow

---
+++ Archivos/LeerArchivoParquet.py
@@ -0,0 +1,30 @@
+import pyarrow.parquet as pq
+import pandas as pd 
+
+
+rutaBase = "d:\\Repositorios\\Git\\Python-utils\\tmp\\"
+
+def leer_parquet(ruta_archivo_parquet):
+    print(f"se leera el archivo {ruta_archivo_parquet}")
+
+    try:
+        
+        pq_file = pq.ParquetFile(ruta_archivo_parquet)
+
+        print("\nMetadatos del archivo Parquet:")
+        print(pq_file.metadata)
+
+        print("\nMetadatos del esquema:")
+        print(pq_file.schema_arrow.metadata)
+
+        table = pq.read_table(ruta_archivo_parquet)
+
+ 
+        print("\nMuestra del Archivo:\n")
+        print(table.to_pandas().head())
+        
+    except Exception as e:
+        print(f" [Error] al leer archivo parquet - {str(e)}")    
+
+
+leer_parquet(rutaBase + "ejemplo_emp.parquet" )
